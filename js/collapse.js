7348a70ada7cb3bb901eff721699ef626b941f94
BingKui <178974662@qq.com>
2015-12-30 17:24:19 +0800

    新增折叠面板

---
+++ js/collapse.js
@@ -0,0 +1,25 @@
+;$(function() {
+	//隐藏所有消息主体内容
+	$('.mc-collapse-content').hide();
+	//点击标题相对应的内容展开，再次点击后折叠
+	$('.mc-item-icon.mc-bottom').bind("touchend", function() {
+		var _con = $(this).next('.mc-collapse-content');
+		if (!_con.hasClass("mc-collapse-show")) {
+			//获取所有的元素
+			var _allCon = $(".mc-list-item div.mc-collapse-content");
+			for (var i = 0; i < _allCon.length; i++) {
+				if ($(_allCon[i]).hasClass("mc-collapse-show")) {
+					$(_allCon[i]).removeClass("mc-collapse-show");
+					$(_allCon[i]).prev(".mc-item-icon").removeClass("mc-top");
+					$(_allCon[i]).slideToggle(400);
+				}
+			}
+			_con.addClass("mc-collapse-show");
+			_con.prev(".mc-item-icon").addClass("mc-top");
+		}else{
+			_con.removeClass("mc-collapse-show");
+			_con.prev(".mc-item-icon").removeClass("mc-top");
+		}
+		_con.slideToggle(400);
+	});
+});
